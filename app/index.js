1923ab9d4d5b68f89f77038c6b27ff8049ecaeb1
seandosborne <seandosborne@gmail.com>
2024-06-26 19:41:52 +1200

    /feat: initial setup page, added link to the login page that is yet to be created. /feat: ColorPalette constant, created and added temporary color values for use whilst testing

--- app/index.js
+++ app/index.js
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
-import { Redirect } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ColorPalette from "../constants/ColorPalette";
 
 export default function InitialSetup() {
   return (
     // if the user is logged in then redirect to the home page
-    <Redirect href={"/(tabs)/home"} />
+    // <Redirect href={"/(tabs)/home"} />
     // else return inital setup page
+    <SafeAreaView style={styles.container}>
+      <Link href="/login" style={styles.primary}>
+        Login
+      </Link>
+    </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: ColorPalette.bg,
+  },
+  primary: {
+    backgroundColor: ColorPalette.primary,
+    color: ColorPalette.primaryFont,
+  },
+});
---
+++ constants/ColorPalette.js
@@ -0,0 +1,10 @@
+// if/else statement for light or dark mode colors
+const ColorPalette = {
+  primary: "blue",
+  secondary: "light-blue",
+  bg: "pink",
+  primaryFont: "white",
+  font: "black",
+};
+
+export default ColorPalette;
