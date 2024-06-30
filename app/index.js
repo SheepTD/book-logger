ad8ec4b49ddbc484214dedfe4cf45be78c88746f
seandosborne <seandosborne@gmail.com>
2024-06-30 15:11:16 +1200

    fix: removed unused imports from the root page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-import { Link, Redirect } from "expo-router";
-import { StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ColorPalette from "../constants/ColorPalette";
 
