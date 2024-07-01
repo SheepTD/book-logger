29d1541e6e6f036e4d522faad4a417452dad7e4c
seandosborne <seandosborne@gmail.com>
2024-07-01 19:09:31 +1200

    /test: Successfully tested Deno edge function for login on mobile but web verstion still returning error.

--- app/index.js
+++ app/index.js
@@ -1,29 +1,62 @@
-import { Link } from "expo-router";
-import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import ColorPalette from "../constants/ColorPalette";
+// import { Link } from "expo-router";
+// import { StyleSheet } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import ColorPalette from "../constants/ColorPalette";
 
-export default function InitialSetup() {
+// export default function InitialSetup() {
+//   return (
+//     // if the user is logged in then redirect to the home page
+//     // <Redirect href={"/(tabs)/home"} />
+//     // else return inital setup page
+//     <SafeAreaView style={styles.container}>
+//       <Link href="/login" style={styles.primary}>
+//         Login
+//       </Link>
+//     </SafeAreaView>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: ColorPalette.bg,
+//   },
+//   primary: {
+//     backgroundColor: ColorPalette.primary,
+//     color: ColorPalette.primaryFont,
+//   },
+// });
+
+// copied from supabase docs & modified to run as JS not TS
+// https://supabase.com/docs/guides/getting-started/tutorials/with-expo-react-native?queryGroups=auth-store&auth-store=async-storage
+
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabase";
+import Auth from "../components/Auth";
+import Account from "../components/Account";
+import { View } from "react-native";
+
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    // if the user is logged in then redirect to the home page
-    // <Redirect href={"/(tabs)/home"} />
-    // else return inital setup page
-    <SafeAreaView style={styles.container}>
-      <Link href="/login" style={styles.primary}>
-        Login
-      </Link>
-    </SafeAreaView>
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Auth />
+      )}
+    </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: ColorPalette.bg,
-  },
-  primary: {
-    backgroundColor: ColorPalette.primary,
-    color: ColorPalette.primaryFont,
-  },
-});
---
+++ components/Auth.js
@@ -0,0 +1,146 @@
+// copied from supabase docs & modified to run as JS not TS
+// https://supabase.com/docs/guides/getting-started/tutorials/with-expo-react-native?queryGroups=auth-store&auth-store=async-storage
+// has been updated to use "log in" not "sign in", have a username option and use the Deno edge function for login
+// needs to be updated to include username option and to not use email verification
+
+import React, { useState } from "react";
+import { Alert, StyleSheet, View, AppState } from "react-native";
+import { supabase } from "../utils/supabase";
+import { Button, Input } from "@rneui/themed";
+
+// Tells Supabase Auth to continuously refresh the session automatically if
+// the app is in the foreground. When this is added, you will continue to receive
+// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
+// if the user's session is terminated. This should only be registered once.
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
+
+export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function logInWithEmail() {
+    setLoading(true);
+
+    // use edge function to log in
+    const { data, error } = await supabase.functions.invoke("login", {
+      body: {
+        identifier: username,
+        password: password,
+      },
+    });
+
+    if (error) Alert.alert(error.message);
+
+    // set session using auth function
+    // It has to be inside it's own function so it can use "error" as a
+    // variable without clashing with the "error" variable for the edge
+    // function.
+    const access_token = data.session.access_token;
+    const refresh_token = data.session.refresh_token;
+
+    const setSession = async (access_token, refresh_token) => {
+      const { error } = await supabase.auth.setSession({
+        access_token: access_token,
+        refresh_token: refresh_token,
+      });
+      if (error) Alert.alert(error.message);
+    };
+    await setSession(access_token, refresh_token);
+
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          username: username,
+        },
+      },
+    });
+
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert(
+        "Code run successfully but session not returned. Try restarting the app and trying again"
+      ); // potentially change this error message
+    setLoading(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Input
+          label="Username"
+          leftIcon={{ type: "font-awesome", name: "user" }}
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+          placeholder="Username"
+          autoCapitalize={"none"}
+        />
+        <Input
+          label="Email"
+          leftIcon={{ type: "font-awesome", name: "envelope" }}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Password"
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button
+          title="Log in"
+          disabled={loading}
+          onPress={() => logInWithEmail()}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button
+          title="Sign up"
+          disabled={loading}
+          onPress={() => signUpWithEmail()}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: "stretch",
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
