c0cef7b2737f90eed87752539158013612cb22ac
SheepTD <seandosborne1@proton.me>
2024-09-17 18:01:44 +1200

    coding session finished: updated stuff using 'npm audit --force' and started to setup ui-datepicker and dayjs in the statistics page.

--- app/(tabs)/statistics.js
+++ app/(tabs)/statistics.js
@@ -1,14 +1,16 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, StyleSheet, Pressable } from "react-native";
+import { StyleSheet } from "react-native";
 import Header from "../../components/Header";
 import { useState } from "react";
 import { TextInput } from "react-native";
 import Size from "../../constants/Size";
 import ColorPalette from "../../constants/ColorPalette";
+import DateTimePicker from "react-native-ui-datepicker";
+import dayjs from "dayjs";
 
 export default function Statistics() {
+  const [endDate, setEndDate] = useState(dayjs()); // set default end date to today
   const [startDate, setStartDate] = useState();
-  const [finishDate, setFinishDate] = useState();
   const [loading, setLoading] = useState(false);
 
   // styles
@@ -35,22 +37,17 @@ export default function Statistics() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header style={{ justifyContent: "space-around" }}>
-        <TextInput
-          value={startDate}
-          onChange={(text) => setStartDate(text)}
-          style={styles.dateInput}
-          disabled={loading}
-          placeholder="start date"
-        />
-        <TextInput
-          value={finishDate}
-          onChange={(text) => setFinishDate(text)}
-          style={styles.dateInput}
-          disabled={loading}
-          placeholder="finish date"
-        />
-      </Header>
+      <Header style={{ justifyContent: "space-around" }}></Header>
+      <DateTimePicker
+        mode="range"
+        startDate={startDate}
+        endDate={endDate}
+        onChange={(params) => {
+          setStartDate(params.startDate);
+          setEndDate(params.endDate);
+        }}
+        firstDayOfWeek={1}
+      />
     </SafeAreaView>
   );
 }
